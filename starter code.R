67a89f6732c38bbe191c8a0e986cf3a970148c9e
Dean Gladish <gladish.dean@gmail.com>
2018-05-30 13:29:48 -0500

    .

--- starter code.R
+++ starter code.R
@@ -1,4 +1,6 @@
 Natality <- read.csv("C:/Users/gladi/Desktop/natality.csv")
 library(MASS)
 mod.base <- glm(Average.Birth.Weight ~ Tobacco.Use + Education.Code + Delivery.Method, data = Natality, family = binomial)
-stepAIC(mod.base)
+mod.step <- stepAIC(mod.base)
+anova(mod.step, test = "Chisq")
+plot(Average.Birth.Weight ~ Education.Code, data = Natality)
